bd69fdf914f20487aae7711c97a04eac60c36f22
Muammarbanana <muammarfanani2@gmail.com>
2021-03-12 13:33:48 +0700

    add custom tabbar

--- lib/tabbar.dart
+++ lib/tabbar.dart
@@ -1,29 +1,48 @@
 import "package:flutter/material.dart";
 
 class TabBarMas extends StatelessWidget {
+  final TabBar myTabBar = TabBar(
+    tabs: [
+      Tab(
+        icon: Icon(Icons.comment),
+        text: "Comment",
+      ),
+      Tab(icon: Icon(Icons.computer)),
+      Tab(text: "News"),
+      Tab(text: "Anime"),
+    ],
+    indicator: BoxDecoration(
+      gradient: LinearGradient(
+        colors: [Colors.blue, Colors.purple],
+      ),
+      border: Border(
+        bottom: BorderSide(color: Colors.white, width: 5),
+      ),
+    ),
+  );
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: DefaultTabController(
         length: 4,
         child: Scaffold(
           appBar: AppBar(
             title: Text("Latihan Tab Bar"),
-            bottom: TabBar(tabs: [
-              Tab(
-                icon: Icon(Icons.comment),
-                text: "Comment",
+            bottom: PreferredSize(
+              preferredSize: Size.fromHeight(myTabBar.preferredSize.height),
+              child: Container(
+                child: myTabBar,
+                color: Colors.lightGreen,
               ),
-              Tab(icon: Icon(Icons.computer)),
-              Tab(text: "News"),
-              Tab(text: "Anime"),
-            ]),
+            ),
           ),
           body: TabBarView(children: [
-            Text("Komentar"),
-            Text("Komputer"),
-            Text("Berita"),
-            Text("Anime"),
+            Center(child: Text("Komentar")),
+            Center(child: Text("Komputer")),
+            Center(child: Text("Berita")),
+            Center(child: Text("Anime")),
           ]),
         ),
       ),
